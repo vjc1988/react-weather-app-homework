0b38feda65943c67c944d4e692ca82a973d9432a
vjc1988 <victoriajaynecadd@yahoo.com>
2025-01-27 17:49:26 +0000

    Added container to App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Victoria Cadd and is{" "}
-        <a
-          href="https://github.com/vjc1988/react-weather-app2/tree/master"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Victoria Cadd and is{" "}
+          <a
+            href="https://github.com/vjc1988/react-weather-app2/tree/master"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
