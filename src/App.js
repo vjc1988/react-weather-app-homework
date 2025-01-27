1666c485674e3c39ac4794ecd13b8945ddb0ec38
vjc1988 <victoriajaynecadd@yahoo.com>
2025-01-27 17:14:00 +0000

    Deleted unnecessary files from React project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Victoria Cadd and is{" "}
+        <a href="">open-sourced</a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
