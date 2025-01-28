5a0e4444a50428de54979111227bca194a805221
vjc1988 <victoriajaynecadd@yahoo.com>
2025-01-28 21:18:26 +0000

    Added HTML/basic CSS to the project

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,44 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Hello</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-6">
+            <input
+              type="search"
+              placeholder="Enter a City ..."
+              className="search-bar"
+            />
+          </div>
+
+          <div className="col-6">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>London</h1>
+      <ul>
+        <li>Wednesday 7:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="mostly cloudy"
+          />
+          6°c
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 32%</li>
+            <li>Wind: 9mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
