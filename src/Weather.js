3a097907ed4c91d264a371ad3f98384afe015f0f
vjc1988 <victoriajaynecadd@yahoo.com>
2025-02-11 17:14:52 +0000

    Started Weather Forecast

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props) {
   const [weatherData, setweatherData] = useState({ ready: false });
@@ -55,6 +56,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast />
       </div>
     );
   } else {
---
+++ src/WeatherForecast.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast(props) {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-Day">Thu</div>
+          <div className="WeatherForecast-Temperatures">
+            <span className="WeatherForecast-Temperature-Max">19</span>
+            <span className="WeatherForecast-Temperature-Min">10</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
