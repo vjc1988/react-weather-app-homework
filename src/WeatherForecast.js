6e9192cfe73ab4dddcda609ef09347e67686360d
vjc1988 <victoriajaynecadd@yahoo.com>
2025-02-17 12:34:23 +0000

    Updated some bugs

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -26,8 +26,11 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
+          ,
         </div>
       </div>
     );
